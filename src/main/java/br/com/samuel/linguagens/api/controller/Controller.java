0f2ad8148dec465c66193df6ffb0a1436c542f8f
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-03-31 11:12:31 -0300

    Status Created

--- src/main/java/br/com/samuel/linguagens/api/controller/Controller.java
+++ src/main/java/br/com/samuel/linguagens/api/controller/Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,8 +36,8 @@ public class Controller {
 	}
 	
 	@PostMapping("/linguagens")
-	public Linguagem cadastrar(@RequestBody Linguagem linguagem) {
-		return repositorio.save(linguagem);
+	public ResponseEntity<Linguagem> cadastrar(@RequestBody Linguagem linguagem) {
+		return new ResponseEntity<>(repositorio.save(linguagem), HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/linguagens/{id}")
